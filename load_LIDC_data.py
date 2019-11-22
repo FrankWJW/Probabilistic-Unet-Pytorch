ec81a511c2cae9e4941d69f2987b74e77e5e3c1b
FrankWJW <541489980@qq.com>
2019-11-22 16:41:01 +0000

    add datadir

--- load_LIDC_data.py
+++ load_LIDC_data.py
@@ -5,6 +5,10 @@ import numpy as np
 import os
 import random
 import pickle
+import matplotlib.pyplot as plt
+
+output_dir = 'D:\LIDC\LIDC-IDRI-out_final'
+
 
 class LIDC_IDRI(Dataset):
     images = []
@@ -29,6 +33,7 @@ class LIDC_IDRI(Dataset):
                 data.update(new_data)
         
         for key, value in data.items():
+            # plt.savefig(os.path.join(output_dir, value['image']))
             self.images.append(value['image'].astype(float))
             self.labels.append(value['masks'])
             self.series_uid.append(value['series_uid'])
