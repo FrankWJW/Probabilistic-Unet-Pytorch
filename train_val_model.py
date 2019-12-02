caaac7905c381de6b4fed2d9a1826b9ec721c4d2
FrankWJW <541489980@qq.com>
2019-12-02 12:39:58 +0000

    hyper para tuning

--- train_val_model.py
+++ train_val_model.py
@@ -24,8 +24,8 @@ model_eval = ''
 resume_model = ''
 
 # hyper para
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-batch_size = 32
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+batch_size = 64
 lr = 1e-4
 weight_decay = 1e-5
 epochs = 300
