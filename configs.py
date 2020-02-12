be53bc0cd252f627f97b3f08a1c1f2e96b45de9f
FrankWJW <541489980@qq.com>
2020-02-12 15:35:07 +0000

    isotropic gaussian from scratch

--- configs.py
+++ configs.py
@@ -8,23 +8,20 @@ import utils.joint_transforms as joint_transforms
 
 print('loading configs.........')
 # --------------------------------Data directory---------------------------------------------------
-# data_dir = '/home/jw7u18/LIDC/data'
-# dir_checkpoint = '/home/jw7u18/probabilistic_unet_output/training_ckpt'
+data_dir = '/home/jw7u18/LIDC/data'
+dir_checkpoint = '/home/jw7u18/probabilistic_unet_output/training_ckpt'
 
-# data_dir = '/home/jw7u18/LIDC/data'
-# dir_checkpoint = '/home/jw7u18/probabilistic_unet_output/training_ckpt'
-
-data_dir = 'D:\Datasets\LIDC\data'
-dir_checkpoint = 'D:\Probablistic-Unet-Pytorch-out\ckpt'
+# data_dir = 'D:\Datasets\LIDC\data'
+# dir_checkpoint = 'D:\Probablistic-Unet-Pytorch-out\ckpt'
 
 recon_dir = 'D:\\Probablistic-Unet-Pytorch-out\\reconstruction_latenDim_6'
 
 # -------------------------------------model dir----------------------------------------------------
-model_eval = 'checkpoint_probUnet_epoch240_latenDim2_totalLoss327366.5144042969_total_reg_loss155248.80108642578_isotropic_False.pth.tar'
+model_eval = ''
 resume_model = ''
 
 # ------------------------------------------training setting---------------------------------------------------
-save_ckpt = False
+save_ckpt = True
 random = False
 r_model = os.path.join(dir_checkpoint, resume_model)
 # -------------------------------------evaluation setting------------------------------------------------------------
@@ -40,19 +37,19 @@ weight_decay = 1e-5
 epochs = 600
 partial_data = False
 resume = False
-latent_dim = 2
-beta = 1
+latent_dim = 6
+beta = 10
 isotropic = False
 
 # kaiming_normal and orthogonal
-# initializers = {'w': 'kaiming_normal', 'b': 'normal'}
-initializers = {'w':None, 'b':None}
+initializers = {'w': 'kaiming_normal', 'b': 'normal'}
+# initializers = {'w':None, 'b':None}
 
 # Transforms
-# joint_transfm = joint_transforms.Compose([joint_transforms.RandomHorizontallyFlip(),
-#                                           joint_transforms.RandomSizedCrop(128),
-#                                           joint_transforms.RandomRotate(60)])
-joint_transfm = None
+joint_transfm = joint_transforms.Compose([joint_transforms.RandomHorizontallyFlip(),
+                                          joint_transforms.RandomSizedCrop(128),
+                                          joint_transforms.RandomRotate(60)])
+# joint_transfm = None
 input_transfm = transforms.Compose([transforms.ToPILImage()])
 target_transfm = transforms.Compose([transforms.ToTensor()])
 # joint_transfm=None
