1d580f107662a375f741fd918ab3547aa8af329c
junwe <541489980@qq.com>
2020-02-14 14:59:38 +0000

    para

--- configs.py
+++ configs.py
@@ -8,14 +8,14 @@ import utils.joint_transforms as joint_transforms
 
 print('loading configs.........')
 # --------------------------------Data directory---------------------------------------------------
-# data_dir = '/home/jw7u18/LIDC/data'
-# dir_checkpoint = '/home/jw7u18/probabilistic_unet_output/training_ckpt'
+data_dir = '/home/jw7u18/LIDC/data'
+dir_checkpoint = '/home/jw7u18/probabilistic_unet_output/training_ckpt'
 
 # data_dir = '/home/jw7u18/LIDC/data'
 # dir_checkpoint = '/home/jw7u18/probabilistic_unet_output/training_ckpt'
 
-data_dir = 'D:\Datasets\LIDC\data'
-dir_checkpoint = 'D:\Probablistic-Unet-Pytorch-out\ckpt'
+# data_dir = 'D:\Datasets\LIDC\data'
+# dir_checkpoint = 'D:\Probablistic-Unet-Pytorch-out\ckpt'
 
 # -------------------------------------model dir----------------------------------------------------
 model_eval = 'checkpoint_probUnet_epoch240_latenDim6_totalLoss28384.866355895996_total_reg_loss154254.47900390625_isotropic_False.pth.tar'
@@ -28,9 +28,9 @@ recon_dir = 'D:\\Probablistic-Unet-Pytorch-out\\reconstructions\\' + model_eval[
 train_or_eval = 'train'
 
 # ------------------------------------------training setting---------------------------------------------------
-save_ckpt = False
+save_ckpt = True
 shuffle_indices = False
-random = False
+random = True
 r_model = os.path.join(dir_checkpoint, resume_model)
 # -------------------------------------evaluation setting------------------------------------------------------------
 num_sample = [1, 4, 8, 16, 50, 100]
@@ -38,7 +38,7 @@ all_experts = False
 eval_model = os.path.join(dir_checkpoint, model_eval)
 # --------------------------------------------------------hyper para-----------------------------------------------
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-num_filters = [32, 64, 128, 256]
+num_filters = [64, 128, 256, 512, 1024]
 batch_size = 32
 lr = 1e-4
 weight_decay = 1e-5
@@ -49,17 +49,17 @@ partial_data = False
 resume = False
 latent_dim = 6
 beta = 10
-# isotropic = False
+isotropic = False
 
 # kaiming_normal and orthogonal
 # initializers = {'w': 'kaiming_normal', 'b': 'normal'}
 initializers = {'w':None, 'b':None}
 
 # Transforms
-# joint_transfm = joint_transforms.Compose([joint_transforms.RandomHorizontallyFlip(),
-#                                           joint_transforms.RandomSizedCrop(128),
-#                                           joint_transforms.RandomRotate(60)])
-joint_transfm = None
+joint_transfm = joint_transforms.Compose([joint_transforms.RandomHorizontallyFlip(),
+                                          joint_transforms.RandomSizedCrop(128),
+                                          joint_transforms.RandomRotate(60)])
+# joint_transfm = None
 input_transfm = transforms.Compose([transforms.ToPILImage()])
 target_transfm = transforms.Compose([transforms.ToTensor()])
 # joint_transfm=None
