4f448f7cb309b580ae7e03af8a28093838b6095d
FrankWJW <541489980@qq.com>
2020-02-17 10:01:56 +0000

    parameter setting

--- configs.py
+++ configs.py
@@ -28,9 +28,9 @@ recon_dir = 'D:\\Probablistic-Unet-Pytorch-out\\reconstructions\\' + model_eval[
 train_or_eval = 'train'
 
 # ------------------------------------------training setting---------------------------------------------------
-save_ckpt = False
+save_ckpt = True
 shuffle_indices = False
-random = False
+random = True
 r_model = os.path.join(dir_checkpoint, resume_model)
 # -------------------------------------evaluation setting------------------------------------------------------------
 num_sample = [1, 4, 8, 16, 50, 100]
@@ -38,7 +38,7 @@ all_experts = False
 eval_model = os.path.join(dir_checkpoint, model_eval)
 # --------------------------------------------------------hyper para-----------------------------------------------
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-num_filters = [32, 64, 128, 256]
+num_filters = [32, 64, 128, 192]
 batch_size = 32
 lr = 1e-4
 weight_decay = 1e-5
@@ -52,14 +52,14 @@ beta = 10
 # isotropic = False
 
 # kaiming_normal and orthogonal
-# initializers = {'w': 'kaiming_normal', 'b': 'normal'}
-initializers = {'w':None, 'b':None}
+initializers = {'w': 'kaiming_normal', 'b': 'normal'}
+# initializers = {'w':None, 'b':None}
 
 # Transforms
-# joint_transfm = joint_transforms.Compose([joint_transforms.RandomHorizontallyFlip(),
-#                                           joint_transforms.RandomSizedCrop(128),
-#                                           joint_transforms.RandomRotate(60)])
-joint_transfm = None
+joint_transfm = joint_transforms.Compose([joint_transforms.RandomHorizontallyFlip(),
+                                          joint_transforms.RandomSizedCrop(128),
+                                          joint_transforms.RandomRotate(60)])
+# joint_transfm = None
 input_transfm = transforms.Compose([transforms.ToPILImage()])
 target_transfm = transforms.Compose([transforms.ToTensor()])
 # joint_transfm=None
